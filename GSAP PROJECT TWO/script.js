59546ce402fb38cd355ca2d82653748485a4846a
WHO AM I ? <114454845+yashYT03@users.noreply.github.com>
2024-11-05 08:13:58 +0530

    best smooth scroll animation in gsap 💡🆕💡

---
+++ GSAP PROJECT TWO/script.js
@@ -0,0 +1,62 @@
+gsap.to(".card1",{
+    scale:0.7,
+    opacity:0,
+    scrollTrigger:{
+        trigger: ".card1",
+        start:"top 15%",
+        end:"bottom 15%",
+        
+        scrub:true,
+
+    } 
+})
+gsap.to(".card2",{
+    scale:0.7,
+    opacity:0,
+    scrollTrigger:{
+        trigger: ".card2",
+        start:"top 15%",
+        end:"bottom 15%",
+       
+        scrub:true,
+
+    } 
+})
+gsap.to(".card3",{
+    scale:0.7,
+    opacity:0,
+    scrollTrigger:{
+        trigger: ".card3",
+        start:"top 15%",
+        end:"bottom 15%",
+        
+        scrub:true,
+
+    } 
+})
+gsap.to(".card4",{
+    scale:0.7,
+    opacity:0,
+    scrollTrigger:{
+        trigger: ".card4",
+        start:"top 15%",
+        end:"bottom 15%",
+       
+        scrub:true,
+
+    } 
+})
+gsap.to(".card5",{
+    scale:0.7,
+    opacity:0,
+    scrollTrigger:{
+        trigger: ".card5",
+        start:"top 15%",
+        end:"bottom 15%",
+       
+        scrub:true,
+
+    } 
+})
+
+
